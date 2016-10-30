fca861d781330b5e7c71a477446f84fdca690bbe
Kristian <krissvor@gmail.com>
2016-10-30 12:46:36 +0100

    knot

--- src/webapp/controllers/SessionsController.php
+++ src/webapp/controllers/SessionsController.php
@@ -6,10 +6,12 @@ use tdt4237\webapp\repository\UserRepository;
 
 class SessionsController extends Controller
 {
+    private $pdo;
 
     public function __construct()
     {
         parent::__construct();
+        $this->pdo = $pdo;
     }
 
     public function newSession()
@@ -25,7 +27,20 @@ class SessionsController extends Controller
     }
 
     public function create()
-    {
+    {   
+        //HERE!
+        $clientIp = $this->get_client_ip();
+        echo '<script>';
+        echo 'console.log("'.$clientIp.'linjafor")';
+        echo '</script>';
+        $q2 = 'SELECT COUNT (LoginId) FROM Logins WHERE ip = "'.$clientIp.'";';
+        $numberOfLogins = self::$pdo->query($q2);
+        echo '<script>';
+        echo 'console.log("antall innlogginger")';
+        echo 'console.log("'.$numberOfLogins.'")';
+        echo '</script>';
+
+
         $request = $this->app->request;
         $user    = $request->post('user');
         $pass    = $request->post('pass');
@@ -46,9 +61,40 @@ class SessionsController extends Controller
             $this->app->redirect('/');
             return;
         }
-
+        $this->invalidLogin();
         $this->app->flashNow('error', 'Incorrect user/pass combination.');
         $this->render('sessions/new.twig', []);
+    }
+
+    public function invalidLogin(){
+     //THIS IS WHERE
+        $ipaddress = $this->get_client_ip();
+        $q1 = 'INSERT INTO logins(ip) VALUES("'.$ipaddress.'");';
+        self::$pdo->exec($q1);
+    }
+        
+
+    //AND HERE
+    public function get_client_ip() {
+        $ipaddress = '';
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
+        echo '<script>';
+        echo 'console.log("'.$ipaddress.'")';
+        echo '</script>';
+        return $ipaddress;
     }
 
     public function destroy()
@@ -56,4 +102,13 @@ class SessionsController extends Controller
         $this->auth->logout();
         $this->app->redirect('http://www.ntnu.no/');
     }
+}
+try {
+    // Create (connect to) SQLite database in file
+    SessionsController::$pdo = new \PDO('sqlite:app.db');
+    // Set errormode to exceptions
+    SessionsController::$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+} catch (\PDOException $e) {
+    echo $e->getMessage();
+    exit();
 }
